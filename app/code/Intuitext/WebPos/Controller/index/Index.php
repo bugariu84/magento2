df717692d839aad47ec570349ea146d3523dd543
bugariu84 <bogdan.bugariu@protonmail.com>
2016-12-15 16:56:48 +0200

    created a basic module

---
+++ app/code/Intuitext/WebPos/Controller/index/Index.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: bbugariu
+ * Date: 12/15/2016
+ * Time: 4:35 PM
+ */
+
+namespace Intuitext\WebPos\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    protected $_resultPageFactory;
+
+    public function __construct(Context $context, PageFactory $resultPageFactory)
+    {
+        $this->_resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * Dispatch request
+     *
+     * @return \Magento\Framework\Controller\ResultInterface|ResponseInterface
+     * @throws \Magento\Framework\Exception\NotFoundException
+     */
+    public function execute()
+    {
+        return $this->_resultPageFactory->create();
+    }
+}
---
+++ app/code/Intuitext/WebPos/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Intuitext_WebPos',
+    __DIR__
+);
